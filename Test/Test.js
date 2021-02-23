44b372073ffbaeefda32c76eeab835034b6141d4
QSmally <github@qbot.eu>
2021-02-23 11:19:04 +0100

    Unit tests: add cookie related tests

--- Test/Test.js
+++ Test/Test.js
@@ -6,3 +6,9 @@ const MyServer = new Serverful(80);
 MyServer.Gateway("public",  Packet => Packet.Request.End(206, "public gateway"));
 MyServer.Gateway("content", Packet => Packet.Request.End(206, "content gateway"));
 MyServer.Gateway("api",     Packet => Packet.Request.End(206, "api gateway"));
+
+MyServer.Incoming(Packet => {
+    console.log(Packet.Cookie());
+    Packet.Cookie("foo", "bar");
+    return Packet.Request.End(200, "cookie foo=bar set");
+});
